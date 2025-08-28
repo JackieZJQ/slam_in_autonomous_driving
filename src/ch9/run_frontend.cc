aadfed2cc23497368d0d0a940a700fc0215c430f
jackie <zhangiaii97@gmail.com>
2025-08-28 17:56:18 +0800

    read run_frontend code

--- src/ch9/run_frontend.cc
+++ src/ch9/run_frontend.cc
@@ -12,18 +12,18 @@
 DEFINE_string(config_yaml, "./config/mapping.yaml", "配置文件");
 
 int main(int argc, char** argv) {
-    google::InitGoogleLogging(argv[0]);
-    FLAGS_stderrthreshold = google::INFO;
-    FLAGS_colorlogtostderr = true;
-    google::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  FLAGS_stderrthreshold = google::INFO;
+  FLAGS_colorlogtostderr = true;
+  google::ParseCommandLineFlags(&argc, &argv, true);
 
-    LOG(INFO) << "testing frontend";
-    sad::Frontend frontend(FLAGS_config_yaml);
-    if (!frontend.Init()) {
-        LOG(ERROR) << "failed to init frontend.";
-        return -1;
-    }
+  LOG(INFO) << "testing frontend";
+  sad::Frontend frontend(FLAGS_config_yaml);
+  if (!frontend.Init()) {
+    LOG(ERROR) << "failed to init frontend.";
+    return -1;
+  }
 
-    frontend.Run();
-    return 0;
+  frontend.Run();
+  return 0;
 }
